d3205d15f6b10b2d9b605cef07d249ea809cb6b1
K899-priya <kp6273275@gmail.com>
2026-02-12 20:52:39 +0530

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Experience from "./pages/Experience";
-import Contact from "./pages/Contact";
+import Navbar from "./component/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Experience from "./pages/Experience.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
   return (
